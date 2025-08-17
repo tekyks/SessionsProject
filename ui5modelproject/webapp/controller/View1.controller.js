d20d6da0b9f8be8a8bd5983b9c6b398cfe935fdc
Krishna Srivastava <teky.ks@gmail.com>
2025-08-18 00:01:54 +0530

    after session 18

--- ui5modelproject/webapp/controller/View1.controller.js
+++ ui5modelproject/webapp/controller/View1.controller.js
@@ -1,6 +1,10 @@
 sap.ui.define(
-  ['sap/ui/core/mvc/Controller', 'sap/ui/model/json/JSONModel'],
-  (Controller, JSONModel) => {
+  [
+    'sap/ui/core/mvc/Controller',
+    'sap/ui/model/json/JSONModel',
+    'sap/ui/model/BindingMode',
+  ],
+  (Controller, JSONModel, BindingMode) => {
     ('use strict');
 
     return Controller.extend('ui5modelproject.controller.View1', {
@@ -9,7 +13,7 @@ sap.ui.define(
         let jsonEmployeeData = {
           employee: {
             id: '1001',
-            name: 'Patrick Sjoman',
+            name: 'Krishna changed value',
             designation: 'Manager',
             department: 'IT',
           },
@@ -17,6 +21,9 @@ sap.ui.define(
 
         //step 2- lets create a JSON Model which will hold employee data
         let jEmpModel = new JSONModel(jsonEmployeeData);
+
+        //Going to change the default behaviour of JSONModel from 2 Way to 1 way binding
+        //jEmpModel.setDefaultBindingMode(BindingMode.OneWay);
 
         //step 3- lets connect json model to our view - View1
         this.getView().setModel(jEmpModel); //default model
@@ -38,7 +45,69 @@ sap.ui.define(
         let jStudentModel = new JSONModel(jsonStudentData);
 
         this.getView().setModel(jStudentModel, 'studentmodel'); //named model
+
+        //-----------------------------------
+        //lets create json object which holds more than 1 employee - demo data
+        let empListJSONData = {
+          employees: [
+            {
+              id: '1001',
+              name: 'Patrick Sjoman',
+              designation: 'Manager',
+              department: 'IT',
+              salary: 10000,
+            },
+            {
+              id: '1002',
+              name: 'Steven Smith',
+              designation: 'Developer',
+              department: 'IT',
+              salary: 20000,
+            },
+            {
+              id: '1003',
+              name: 'Patrick Kelly',
+              designation: 'Admin',
+              department: 'Admin',
+              salary: 25000,
+            },
+            {
+              id: '1004',
+              name: 'Tony Richman',
+              designation: 'Senior Manager',
+              department: 'HR',
+              salary: 30000,
+            },
+            {
+              id: '1005',
+              name: 'Ajanta Mendis',
+              designation: 'Developer',
+              department: 'IT',
+              salary: 40000,
+            },
+            {
+              id: '1006',
+              name: 'Krishna',
+              designation: 'Developer',
+              department: 'IT',
+              salary: 50000,
+            },
+          ],
+        };
+
+        let jEmpListModel = new JSONModel(empListJSONData);
+
+        this.getView().setModel(jEmpListModel, 'emplistmodel'); //named model
       },
     });
   }
 );
+
+// if (salary <= 25000){
+//   console.log("Less Salary employee");
+// }else{
+//   console.log('High Salary employee');
+// }
+//ternary operator
+// let message = salary <= 25000 ? 'Less Salary employee' : 'High Salary employee';
+// console.log(message);
